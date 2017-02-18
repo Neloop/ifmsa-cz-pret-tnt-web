ac6d9f78fb2184286559f5b4c0e9348c56a7e82c
Martin Polanka <PolankaMartin@gmail.com>
2017-02-18 10:29:26 +0100

    Forms rendering and small tweaks

--- app/forms/RegistrationFormsFactory.php
+++ app/forms/RegistrationFormsFactory.php
@@ -32,14 +32,6 @@ class RegistrationFormsFactory
         $this->registrationEmailsSender = $registrationEmailsSender;
     }
 
-    private function getAllergiesItems()
-    {
-        return array(
-            "no" => "No",
-            "yes" => "Yes:"
-        );
-    }
-
     private function getDietItems()
     {
         return array(
@@ -50,34 +42,30 @@ class RegistrationFormsFactory
         );
     }
 
-    private function getEventsAttendedItems()
-    {
-        return array(
-            "none" => "None",
-            "some" => "Some:"
-        );
-    }
-
     private function createBasicRegistrationForm(): BootstrapForm
     {
         $form = new BootstrapForm();
 
         $form->addText("firstname", $this->labelsHelper->getLabel("firstname"))
                 ->setRequired("Firstname is required")
+                ->setAttribute("placeholder", $this->labelsHelper->getLabel("firstname"))
                 ->addRule(Form::MAX_LENGTH, "Firstname is too long", 255);
         $form->addText("surname", $this->labelsHelper->getLabel("surname"))
                 ->setRequired("Surname is required")
+                ->setAttribute("placeholder", $this->labelsHelper->getLabel("surname"))
                 ->addRule(Form::MAX_LENGTH, "Surname is too long", 255);
         $form->addText("email", $this->labelsHelper->getLabel("email"))
+                ->setAttribute("placeholder", $this->labelsHelper->getLabel("email"))
                 ->setRequired("Email is required")
                 ->addRule(Form::MAX_LENGTH, "Email is too long", 255)
                 ->addRule(Form::EMAIL, "Email is not in the right format");
         $form->addText("phone", $this->labelsHelper->getLabel("phone"))
                 ->setRequired("Phone is required")
+                ->setAttribute("placeholder", $this->labelsHelper->getLabel("phone"))
                 ->addRule(Form::MAX_LENGTH, "Phone is too long", 255);
-        $form->addRadioList("allergies", $this->labelsHelper->getLabel("allergies"), $this->getAllergiesItems())
-                ->setRequired("Allergies are required");
-        $form->addText("allergiesText")->setRequired(false)
+        $form->addText("allergies", $this->labelsHelper->getLabel("allergies"))
+                ->setRequired(false)
+                ->setAttribute("placeholder", "None")
                 ->addRule(Form::MAX_LENGTH, "Allergies text is too long", 1000);
         $form->addRadioList("diet", $this->labelsHelper->getLabel("diet"), $this->getDietItems())
                 ->setRequired("Diet is required");
@@ -88,20 +76,24 @@ class RegistrationFormsFactory
 
         $form->addText("nmo", $this->labelsHelper->getLabel("nmo"))
                 ->setRequired("NMO is required")
+                ->setAttribute("placeholder", "National Member Organization")
                 ->addRule(Form::MAX_LENGTH, "NMO is too long", 255);
         $form->addText("nmoPosition", $this->labelsHelper->getLabel("nmoPosition"))
                 ->setRequired("NMO Position is required")
+                ->setAttribute("placeholder", "Position in National Member Organization")
                 ->addRule(Form::MAX_LENGTH, "NMO Position is too long", 255);
-        $form->addRadioList("ifmsaEventsAttended", $this->labelsHelper->getLabel("ifmsaEventsAttended"), $this->getEventsAttendedItems())
-                ->setRequired("IFMSA events attended field is required");
-        $form->addText("ifmsaEventsAttendedText")->setRequired(false)
+        $form->addTextArea("ifmsaEventsAttended", $this->labelsHelper->getLabel("ifmsaEventsAttended"))
+                ->setRequired(false)
+                ->setAttribute("placeholder", "None")
                 ->addRule(Form::MAX_LENGTH, "IFMSA events attended text is too long", 1000);
 
         $form->addText("universityFaculty", $this->labelsHelper->getLabel("universityFaculty"))
                 ->setRequired("University/Faculty is required")
+                ->setAttribute("placeholder", $this->labelsHelper->getLabel("universityFaculty"))
                 ->addRule(Form::MAX_LENGTH, "University/Faculty is too long", 255);
         $form->addText("city", $this->labelsHelper->getLabel("city"))
                 ->setRequired("City is required")
+                ->setAttribute("placeholder", $this->labelsHelper->getLabel("city"))
                 ->addRule(Form::MAX_LENGTH, "City is too long", 255);
         $form->addText("yearOfStudy", $this->labelsHelper->getLabel("yearOfStudy"))
                 ->setType("number")->setDefaultValue(1)
@@ -109,9 +101,12 @@ class RegistrationFormsFactory
                 ->addRule(Form::INTEGER, "Year of Study has to be integer")
                 ->addRule(Form::RANGE, "Year of Study has to be number from 1 to 6", [1, 6]);
 
-        $form->addText("motivation", $this->labelsHelper->getLabel("motivation"))
+        $form->addTextArea("motivation", $this->labelsHelper->getLabel("motivation"))
                 ->setRequired("Motivation is required")
+                ->setAttribute("placeholder", "Motivation")
                 ->addRule(Form::MAX_LENGTH, "Motivation is too long", 1000);
+
+        $form->addReCaptcha('captcha', null, "Please prove you're not a robot.");
 
         return $form;
     }
@@ -119,7 +114,6 @@ class RegistrationFormsFactory
     public function createPretRegistrationForm(): BootstrapForm
     {
         $form = $this->createBasicRegistrationForm();
-        $form->addReCaptcha('captcha', NULL, "Please prove you're not a robot.");
         $form->addSubmit('send', 'Register');
         $form->onSuccess[] = array($this, 'pretRegistrationFormSucceeded');
         return $form;
@@ -131,15 +125,17 @@ class RegistrationFormsFactory
 
         $form->addTextArea("tntStrengthsAsTrainer", $this->labelsHelper->getLabel("tntStrengthsAsTrainer"))
                 ->setRequired("Strengths are required")
+                ->setAttribute("placeholder", "Strengths as Trainer")
                 ->addRule(Form::MAX_LENGTH, "Strengths are too long", 1200);
         $form->addTextArea("tntVisionAsTrainer", $this->labelsHelper->getLabel("tntVisionAsTrainer"))
                 ->setRequired("Vision is required")
+                ->setAttribute("placeholder", "Vision as Trainer")
                 ->addRule(Form::MAX_LENGTH, "Vision is too long", 600);
         $form->addTextArea("tntUsageOfKnowledge", $this->labelsHelper->getLabel("tntUsageOfKnowledge"))
                 ->setRequired("Usage of Knowledge is required")
+                ->setAttribute("placeholder", "Usage of Gained Knowledge")
                 ->addRule(Form::MAX_LENGTH, "Usage of Knowledge too long", 1000);
 
-        $form->addReCaptcha('captcha', NULL, "Please prove you're not a robot.");
         $form->addSubmit('send', 'Register');
         $form->onSuccess[] = array($this, 'tntRegistrationFormSucceeded');
         return $form;
@@ -155,30 +151,12 @@ class RegistrationFormsFactory
         return true;
     }
 
-    private function getAllergiesFromValues($values): string
-    {
-        if ($values->allergies === "yes") {
-            return $values->allergiesText;
-        } else {
-            return $this->getAllergiesItems()[$values->allergies];
-        }
-    }
-
     private function getDietFromValues($values): string
     {
         if ($values->diet === "another") {
             return $values->dietText;
         } else {
             return $this->getDietItems()[$values->diet];
-        }
-    }
-
-    private function getEventsAttendedFromValues($values): string
-    {
-        if ($values->ifmsaEventsAttended === "some") {
-            return $values->ifmsaEventsAttendedText;
-        } else {
-            return $this->getEventsAttendedItems()[$values->ifmsaEventsAttended];
         }
     }
 
@@ -189,13 +167,13 @@ class RegistrationFormsFactory
             $values->surname,
             $values->email,
             $values->phone,
-            $this->getAllergiesFromValues($values),
+            $values->allergies,
             $this->getDietFromValues($values),
             $values->visaNeeded,
             $values->invitationLetterNeeded,
             $values->nmo,
             $values->nmoPosition,
-            $this->getEventsAttendedFromValues($values),
+            $values->ifmsaEventsAttended,
             $values->universityFaculty,
             $values->city,
             $values->yearOfStudy,
