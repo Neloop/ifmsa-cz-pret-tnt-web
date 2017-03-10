c2af2c38f3755012df114a43e23793e57f454d20
Martin Polanka <PolankaMartin@gmail.com>
2017-03-10 21:05:57 +0100

    Payment details email content

--- app/helpers/emails/PaymentEmailsSender.php
+++ app/helpers/emails/PaymentEmailsSender.php
@@ -44,10 +44,31 @@ class PaymentEmailsSender
     {
         $subject = strtoupper($participant->pretOrTnt) . " Payment Details";
 
-        $message = "<p>Dear IFMSA friend,</p>";
-        $message .= "<p>thank you again for your registration. We have great news for you, you were chosen to participate PRET&TNT Prague 2017. There is only one final step, payment for the event.</p>";
-        $message .= "<p>Your registration will be recognized as binding if you execute payment through our payment gateway which resides <a href='{$this->appParams->getParticipantPaymentUrl()}/{$participant->id}' target='_blank'>here</a>.</p>";
-        $message .= "<p>And this is it, we will look forward to meet you in Prague!</p>";
+        $message = "Dear applicant,<br>";
+        $message .= "Thank you very much for your interest in PRET&TNT Prague"
+                . " 2017<br>";
+        $message .= "It is our pleasure to inform you that you <u>have been"
+                . " selected</u> and you are invited to attend the event!<br>";
+        $message .= "Congratulations!<br>";
+        $message .= "<br>";
+
+        $message .= "We would like to ask you to pay the participation fee in"
+                . " <u>five days</u> since you receive this email. Please be"
+                . " aware that your registration will be recognized valid as"
+                . " soon as you proceed the payment. Payment is realised"
+                . " through our payment gateway and you can pay with your"
+                . " Master Card or Visa. Link for the payment is below:<br>";
+        $paymentLink = $this->appParams->getParticipantPaymentUrl() . "/" . $participant->id;
+        $message .= "<a href='{$paymentLink}' target='_blank'>{$paymentLink}</a><br>";
+        $message .= "<br>";
+
+        $message .= "Applicants with visa requirement, please contact us at"
+                . " <a href='mailto:neo@ifmsa.cz'>neo@ifmsa.cz</a> with your"
+                . " specific needs for visa process with enclosed copy of your"
+                . " passport<br>";
+        $message .= "Don't forget to put going on our"
+                . " <a href='{$this->appParams->getFacebookEventUrl()}' target='_blank'>facebook event</a>"
+                . " to receive new updates about the event!<br>";
 
         return $this->emailHelper->send([$participant->email], $subject, $message);
     }
