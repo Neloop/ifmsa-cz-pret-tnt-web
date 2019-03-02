7d33e4d22e78e4a1a1ff4009536d7e6e9f9e18b5
Martin Polanka <PolankaMartin@gmail.com>
2019-03-02 22:32:49 +0100

    Add https proxy hack needed for active24 hosting

--- www/index.php
+++ www/index.php
@@ -1,5 +1,17 @@
 <?php
 
+// Hack for https proxies needed for example on active24 hosting, which has
+// nginx as reverse proxy communicating unsecurely with Apache
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO'])) {
+    if ($_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https' && isset($_SERVER['SERVER_PORT']) && in_array($_SERVER['SERVER_PORT'], [80, 82])) { // https over proxy
+        $_SERVER['HTTPS'] = 'On';
+        $_SERVER['SERVER_PORT'] = 443;
+    } elseif ($_SERVER['HTTP_X_FORWARDED_PROTO'] === 'http' && isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == 80) { // http over proxy
+        $_SERVER['HTTPS'] = 'Off';
+        $_SERVER['SERVER_PORT'] = 80;
+    }
+}
+
 // Uncomment this line if you must temporarily take down your site for maintenance.
 // require __DIR__ . '/.maintenance.php';
 
